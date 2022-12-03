b53b00c0af695aa7e4d7235df11855b1007e75d1
lian <1013312097@qq.com>
2022-12-03 10:46:34 +0800

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,83 @@
+import {
+  defineConfig
+} from 'vitepress'
+
+export default {
+  title: 'VitePress',
+  description: '练练不会前端.',
+  head: [['meta', { name: 'keywords', content: 'HTML, CSS, JavaScript' }]],
+  themeConfig: {
+    logo: '/tuzi.png',
+    siteTitle: '练练',
+    docFooter: { prev: '上一篇', next: '下一篇' },
+    nav: [{
+        text: "指南",
+        link: "/guild/1",
+      },
+      {
+        text: "组件",
+        link: "/examples/3"
+      },
+      {
+        text: '下拉选择框',
+        items: [
+          { text: 'options-1', link: '/' },
+          { text: 'options-2', link: 'http://www.baidu.com' }
+        ]
+      }
+    ],
+    sidebar: {
+      "/guild/": [
+        {
+          text: "jsday1",
+          items: [
+            {
+              text: "安装",
+              link: "/guild/JSday1",
+            },
+            {
+              text: "快速开始",
+              link: "/guild/quickstart",
+            },
+          ],
+        },
+        {
+          text: "进阶",
+          items: [
+            {
+              text: "xx",
+              link: "/xx",
+            },
+          ],
+        },
+      ],
+      "/examples/": [
+        {
+          text: "测试内容",
+          items: [
+            {
+              text: "滴滴滴滴",
+              link: "/examples/1",
+            },
+            {
+              text: "大大大的",
+              link: "/examples/2",
+            },
+          ],
+        },
+      ],
+      
+    },
+    socialLinks: [{
+      icon: "github",
+      link: "https://gitee.com/geeksdidi"
+    }],
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2019-present Evan You'
+    }
+
+  },
+
+
+}
---
+++ docs/index.md
@@ -0,0 +1,37 @@
+---
+layout: home
+
+hero:
+  name: 练练不会前端
+  text: 哈哈哈O(∩_∩)O哈哈~
+  tagline: 更多内容正在加班中...
+  Image: /faVicon.ico
+  image:
+    src: /electro.png
+    alt: 嘻嘻嘻
+  actions:
+    - theme: brand
+      text: 基础班
+      link: /
+    - theme: alt
+      text: 移动web
+      link: 
+    - theme: brand
+      text: JavaScript基础
+      link: /
+    - theme: alt
+      text: web APIs
+      link: 
+
+features:
+  - icon: 💟
+    title: 课堂资料
+    details: 基础班/移动web/JavaScript基础/web apis
+  - icon: ✍
+    title: 前端面试题
+    details: 前端常见的面试题
+  - icon: 💗
+    title: 欢迎指出问题
+    details: phone:18687134197
+---
+
